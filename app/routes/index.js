21fc0beb256001fe4a165c6fde1d4b5a6b2bffea
Ash Laidlaw and Carlos Munoz Kampff <carlos@>
2017-03-21 16:41:36 -0700

    add dynamic url links to mtv articles.

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,10 +2,12 @@ import Ember from 'ember';
 
 var test = [];
 
+
 $.get("https://newsapi.org/v1/articles?source=mtv-news&sortBy=latest&apiKey=114a62f01a5049e79f27797505873305").then(function(response) {
   for (var i = 0; i < response.articles.length; i++) {
     test[i] = {
       title: response.articles[i].title,
+      url: response.articles[i].url,
       author: response.articles[i].author,
       body: response.articles[i].description,
       img_link: response.articles[i].urlToImage
