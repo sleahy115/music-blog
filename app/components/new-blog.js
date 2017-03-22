4dfe50702bddcdb7bf2d67fb95583eca879ace7a
Carlos Munoz Kampff and Sarah Leahy <carlos@>
2017-03-22 14:14:24 -0700

    complete create new blog functionality.

--- app/components/new-blog.js
+++ app/components/new-blog.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  showNewBlogForm: false,
+  actions: {
+    showNewBlogForm() {
+      this.set('showNewBlogForm', true);
+    },
+
+    saveNewBlog() {
+      var params = {
+        author: this.get('author'),
+        body: this.get('body'),
+        group: this.get('group'),
+        img_link: this.get('img_link'),
+        publish_date: this.get('publish_date'),
+        title: this.get('title'),
+      };
+      this.set('addNewBlog', false);
+      this.sendAction('saveNewBlog', params);
+    }
+  }
 });
--- app/routes/blog.js
+++ app/routes/blog.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(){
     return this.store.findAll('blogPost');
+  },
+  actions: {
+    saveNewBlog(params) {
+    var newBlog = this.store.createRecord('blogPost', params);
+    newBlog.save();
+    this.transitionTo('blog');
+    }
   }
 });
