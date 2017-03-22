e65faff9006f25b1e1e6608922c30a73e93b2560
Carlos Munoz Kampff and Sarah Leahy <carlos@>
2017-03-22 11:55:00 -0700

    add new blog form

--- app/components/blog-tile.js
+++ app/components/blog-tile.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewBlog: false,
+  actions: {
+    showNewBlogForm() {
+      this.set('addNewBlog', true);
+    },
+
+    saveBlog() {
+      var params = {
+        title: this.get('title'),
+        date: this.get('date'),
+        author: this.get('author'),
+        img_src: this.get('img_src'),
+        publish_date: this.get('publish_date'),
+        body: this.get('body')
+        description: this.get('description')
+      };
+      this.set('addNewBlog', false);
+      this.sendAction('saveBlog', params);
+    }
+  }
+});
+
 });
