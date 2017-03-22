e334036698fa417fa9f6ed8099f990d6379f7a6e
Carlos Munoz Kampff and Sarah Leahy <carlos@>
2017-03-22 15:47:03 -0700

    complete delete functionality.

--- app/routes/blog-page.js
+++ app/routes/blog-page.js
@@ -3,5 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('blogPost', params.blogPost_id);
+  },
+  actions: {
+    delete(blogPost) {
+      if(confirm('are you sure that you want to delete this blog post?')) {
+        blogPost.destroyRecord();
+        this.transitionTo('blog');
+      }
+    }
   }
 });
